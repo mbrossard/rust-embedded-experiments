3768fbc02db9978dc5dd79e1f9abf933fbedb43f
Mathias Brossard <mathias@brossard.org>
2020-03-11 00:33:22 -0500

    Make use of core delay a feature

--- stm32f411-hal-blinky/src/main.rs
+++ stm32f411-hal-blinky/src/main.rs
@@ -12,24 +12,37 @@ use hal::prelude::*;
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let dev_peripherals = hal::stm32::Peripherals::take().unwrap();
-    let core_peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
 
     // Set up the LED connected to pin PC13.
     let gpioc = dev_peripherals.GPIOC.split();
     let mut led = gpioc.pc13.into_push_pull_output();
 
-    // Set up the system clock at 100MHz.
-    let rcc = dev_peripherals.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(100.mhz()).freeze();
+    #[cfg(not(feature = "core_delay"))]
+    let mut delay: hal::delay::Delay = {
+        let core_peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
 
-    // Create a delay abstraction based on SysTick
-    let mut delay = hal::delay::Delay::new(core_peripherals.SYST, clocks);
+        // Set up the system clock at 100MHz.
+        let rcc = dev_peripherals.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(100.mhz()).freeze();
 
+        // Create a delay abstraction based on SysTick
+        hal::delay::Delay::new(core_peripherals.SYST, clocks)
+    };
+
+    let mut on = true;
     loop {
-        // On for 1s, off for 1s.
-        led.set_high().unwrap();
+        if on {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+
+        #[cfg(feature = "core_delay")]
+        cortex_m::asm::delay(10000000);
+
+        #[cfg(not(feature = "core_delay"))]
         delay.delay_ms(1000_u32);
-        led.set_low().unwrap();
-        delay.delay_ms(1000_u32);
+
+        on = !on;
     }
 }
