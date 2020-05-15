eb5f5ce886eac189232edb142888e499cccbf6b3
Mathias Brossard <mathias@brossard.org>
2020-05-14 23:52:09 -0500

    Adding ads1115 example for stm32f411

---
+++ stm32f411/ads1115-i2c/src/main.rs
@@ -0,0 +1,71 @@
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+extern crate cortex_m_rt;
+extern crate panic_semihosting;
+extern crate cortex_m_semihosting;
+#[macro_use(block)]
+extern crate nb;
+
+use cortex_m_semihosting::{hprintln, dbg, hio};
+use core::fmt::Write;
+use stm32f4xx_hal::{i2c::I2c, delay::Delay, prelude::*, stm32};
+use ads1x1x::{channel::*, Ads1x1x, SlaveAddr};
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    // cortex_m::asm::delay(1000000000);
+
+    if let (Some(dp), Some(cp)) = (
+        stm32::Peripherals::take(),
+        cortex_m::peripheral::Peripherals::take(),
+    ) {
+        // Set up the system clock. We want to run at 48MHz for this one.
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+        // Set up the LED connected to pin PC13.
+        let gpioc = dp.GPIOC.split();
+        let mut led = gpioc.pc13.into_push_pull_output();
+
+        // Set up I2C - SCL is PB6 and SDA is PB7; they are set to Alternate Function 4
+        // as per the STM32F411xC/E datasheet page 48.
+        let gpiob = dp.GPIOB.split();
+
+        let mut delay = Delay::new(cp.SYST, clocks);
+
+        let scl = gpiob.pb6.into_alternate_af4().set_open_drain();
+        let sda = gpiob.pb7.into_alternate_af4().set_open_drain();
+
+        let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 100.khz(), clocks);
+
+        let mut stdout = hio::hstdout().unwrap();
+        writeln!(stdout, "Hello world!").ok();
+
+        // initialize the ADC
+        let mut adc = Ads1x1x::new_ads1015(i2c, SlaveAddr::default());
+
+        writeln!(stdout, "Hello world!").ok();
+        let mut on = true;
+        loop {
+            if on {
+                led.set_high().unwrap();
+            } else {
+                led.set_low().unwrap();
+            }
+
+            hprintln!("Measurements: [{}, {}, {}, {}]",
+                block!(adc.read(&mut SingleA0)).unwrap(),
+                block!(adc.read(&mut SingleA1)).unwrap(),
+                block!(adc.read(&mut SingleA2)).unwrap(),
+                block!(adc.read(&mut SingleA3)).unwrap()).ok();
+
+            delay.delay_ms(1000_u32);
+
+            on = !on;
+        }
+    }
+
+    loop {}
+}
