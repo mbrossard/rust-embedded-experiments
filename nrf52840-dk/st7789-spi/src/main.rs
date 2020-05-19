28f3bc173149af72ddc1edbb7b2a993e4d01e96f
Mathias Brossard <mathias@brossard.org>
2020-05-18 22:48:58 -0500

    Draw ferris multiple times and match pins with documentation

--- nrf52840-dk/st7789-spi/src/main.rs
+++ nrf52840-dk/st7789-spi/src/main.rs
@@ -27,31 +27,45 @@ fn main() -> ! {
     let spiclk = pins1.p1_14.into_push_pull_output(Level::Low).degrade();
     let spimosi = pins1.p1_13.into_push_pull_output(Level::Low).degrade();
     let spimiso = pins1.p1_12.into_floating_input().degrade();
-    let _cs = pins1.p1_11.into_push_pull_output(Level::Low).degrade();
     let rst = pins1.p1_08.into_push_pull_output(Level::Low).degrade();
     let dc = pins1.p1_07.into_push_pull_output(Level::Low).degrade();
+    let _cs = pins1.p1_06.into_push_pull_output(Level::Low).degrade();
 
     let spi_pins = nrf52840_hal::spim::Pins { sck: spiclk, miso: Some(spimiso), mosi: Some(spimosi) };
-    let spi = Spim::new(p.SPIM0, spi_pins, Frequency::K500, MODE_0, 0);
+    let spi = Spim::new(p.SPIM0, spi_pins, Frequency::M8, MODE_3, 122);
 
     let delay = Delay::new(cp.SYST);
     let mut display = ST7789::new(spi, dc, rst, 240, 240, delay);
-    display.hard_reset().unwrap();
-    display.set_orientation(&Orientation::Landscape).unwrap();
-    display.init().unwrap();
 
+    display.init().unwrap();
+    display.set_orientation(&Orientation::Portrait).unwrap();
+    display.clear(Rgb565::BLACK).unwrap();
 
     let mut rng = SmallRng::seed_from_u64(0);
     let raw_image_data = ImageRawLE::new(include_bytes!("./ferris.raw"), 86, 64);
-    let ferris = Image::new(&raw_image_data, Point::new(34, 8));
-
     let black = Rgb565::BLACK.into_storage();
     let white = Rgb565::WHITE.into_storage();
     let mut img = true;
+
     loop {
         if img {
-            ferris.draw(&mut display).unwrap();
-            // display.flush().unwrap();
+            display.clear(Rgb565::BLACK).unwrap();
+            let mut xx: u16 = 0;
+            let mut yy: u16 = 0;
+
+            loop {
+                let ferris = Image::new(&raw_image_data, Point::new(xx as i32, yy as i32));
+                ferris.draw(&mut display).unwrap();
+
+                xx += 86;
+                if xx + 86 >= 240 {
+                    xx = 0;
+                    yy += 64;
+                }
+                if yy + 64 >= 240 {
+                    break;
+                }
+            }
         } else {
             let width: u16 = 240;
             let height: u16 = 240;
@@ -97,7 +111,6 @@ fn main() -> ! {
                 }
                 y += i_size * p_size + interval;
             }
-            // display.flush().unwrap();
         }
         cortex_m::asm::delay(50000000);
         img = !img;
