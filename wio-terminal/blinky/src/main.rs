dfb6830b969a13cc019c5afafa7c3b841f52c406
Mathias Brossard <mathias@brossard.org>
2020-08-11 21:54:27 -0500

    Use git version of atsamd-hal to get samd51p19a support

--- wio-terminal/blinky/src/main.rs
+++ wio-terminal/blinky/src/main.rs
@@ -1,21 +1,31 @@
 #![no_std]
 #![no_main]
 
-extern crate feather_m4;
 extern crate panic_halt;
+extern crate atsamd_hal as hal;
 
-use feather_m4::prelude::*;
+use hal::prelude::*;
+use hal::*;
 
-#[feather_m4::entry]
+use gpio::{Floating, Input, Port};
+
+define_pins!(
+    struct Pins,
+    target_device: target_device,
+
+    pin led = a15,
+);
+
+#[cortex_m_rt::entry]
 fn main() -> ! {
-    let peripherals = feather_m4::pac::Peripherals::take().unwrap();
-    let mut pins = feather_m4::Pins::new(peripherals.PORT);
-    let mut led = pins.d13.into_open_drain_output(&mut pins.port);
+    let peripherals = atsamd_hal::target_device::Peripherals::take().unwrap();
+    let mut pins = Pins::new(peripherals.PORT);
+    let mut led = pins.led.into_open_drain_output(&mut pins.port);
 
     loop {
-        led.set_high().unwrap();
-        cortex_m::asm::delay(5000000);
-        led.set_low().unwrap();
-        cortex_m::asm::delay(5000000);
+         led.set_high().unwrap();
+         cortex_m::asm::delay(5000000);
+         led.set_low().unwrap();
+         cortex_m::asm::delay(5000000);
     }
 }
