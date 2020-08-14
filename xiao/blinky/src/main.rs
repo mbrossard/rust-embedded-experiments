e45b705aafcb8df917efc9a3cb0f7d2e3e21ec34
Mathias Brossard <mathias@brossard.org>
2020-08-13 21:25:59 -0500

    Add xiao M0 blinky

---
+++ xiao/blinky/src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate xiao_m0 as hal;
+
+use hal::prelude::*;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    let peripherals = hal::pac::Peripherals::take().unwrap();
+    let mut pins = hal::Pins::new(peripherals.PORT);
+    let mut led0 = pins.led0.into_open_drain_output(&mut pins.port);
+    let mut led1 = pins.led1.into_open_drain_output(&mut pins.port);
+    let mut led2 = pins.led2.into_open_drain_output(&mut pins.port);
+
+    led0.set_high().unwrap();
+    led1.set_high().unwrap();
+    led2.set_high().unwrap();
+
+    loop {
+         led0.set_low().unwrap();
+         cortex_m::asm::delay(500000);
+         led0.set_high().unwrap();
+         led1.set_low().unwrap();
+         cortex_m::asm::delay(500000);
+         led1.set_high().unwrap();
+         led2.set_low().unwrap();
+         cortex_m::asm::delay(500000);
+         led2.set_high().unwrap();
+    }
+}
