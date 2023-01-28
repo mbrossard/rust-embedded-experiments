5605b55efaebd82597e0a089720ee0bca2df1388
Mathias Brossard <mathias@brossard.org>
2023-01-27 22:18:23 -0600

    Update crates for xiao blinky

--- xiao/blinky/src/main.rs
+++ xiao/blinky/src/main.rs
@@ -4,29 +4,29 @@
 extern crate panic_halt;
 extern crate xiao_m0 as hal;
 
-use hal::prelude::*;
+use hal::hal::prelude::*;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let peripherals = hal::pac::Peripherals::take().unwrap();
-    let mut pins = hal::Pins::new(peripherals.PORT);
-    let mut led0 = pins.led0.into_open_drain_output(&mut pins.port);
-    let mut led1 = pins.led1.into_open_drain_output(&mut pins.port);
-    let mut led2 = pins.led2.into_open_drain_output(&mut pins.port);
+    let pins = hal::Pins::new(peripherals.PORT);
+    let mut led0 = pins.led0.into_push_pull_output();
+    let mut led1 = pins.led1.into_push_pull_output();
+    let mut led2 = pins.led2.into_push_pull_output();
 
     led0.set_high().unwrap();
     led1.set_high().unwrap();
     led2.set_high().unwrap();
 
     loop {
-         led0.set_low().unwrap();
-         cortex_m::asm::delay(500000);
-         led0.set_high().unwrap();
-         led1.set_low().unwrap();
-         cortex_m::asm::delay(500000);
-         led1.set_high().unwrap();
-         led2.set_low().unwrap();
-         cortex_m::asm::delay(500000);
-         led2.set_high().unwrap();
+        led0.set_low().unwrap();
+        cortex_m::asm::delay(500000);
+        led0.set_high().unwrap();
+        led1.set_low().unwrap();
+        cortex_m::asm::delay(500000);
+        led1.set_high().unwrap();
+        led2.set_low().unwrap();
+        cortex_m::asm::delay(500000);
+        led2.set_high().unwrap();
     }
 }
